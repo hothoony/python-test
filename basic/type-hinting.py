05b089730ed37d1e6bb87d24ca44b8177f839a17
hothoony <hothoony@hanmail.net>
2024-08-28 15:37:29 +0900

    .

---
+++ basic/type-hinting.py
@@ -0,0 +1,26 @@
+
+name: str = "paul"
+age: int = 20
+height: float = 3.2
+isSuccess: bool = True
+print()
+print(name)
+print(age)
+print(height)
+print(isSuccess)
+
+name = "james"
+age = 30
+height = 2.3
+isSuccess = False
+print()
+print(name)
+print(age)
+print(height)
+print(isSuccess)
+
+def sum(a: int, b:int) -> int:
+    return a + b
+
+print()
+print(sum(11, 22))
