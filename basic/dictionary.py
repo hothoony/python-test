49653d78454f13c5089b8d8ad4667d33a288c7bb
hothoony <hothoony@hanmail.net>
2024-08-28 10:33:53 +0900

    .

---
+++ basic/dictionary.py
@@ -0,0 +1,15 @@
+# 더블 따옴표 사용
+dict1 = {
+    "first": "hue",
+    "last": "jackman",
+    "year": 2024,
+}
+print(dict1)
+
+# 싱글 따옴표 사용
+dict2 = {
+    'first': 'hue',
+    'last': 'jackman',
+    'year': 2024,
+}
+print(dict2)
