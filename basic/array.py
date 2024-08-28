ea582b638cb86877be6fc6e5576609029b468e31
hothoony <hothoony@hanmail.net>
2024-08-28 10:04:08 +0900

    .

---
+++ basic/array.py
@@ -0,0 +1,9 @@
+# 숫자 배열
+list = [11, 22, 33]
+for i in list:
+    print(i)
+
+# 문자 배열
+list = ['aa', 'bb', 'cc']
+for i in list:
+    print(i)
--- basic/variable.py
+++ basic/variable.py
@@ -1,13 +1,18 @@
 
+# 문자
 msg = "variable test"
 print(msg)
 
+
+# 숫자
 msg = 123
 print(msg)
 
 msg = 10 + 20
 print(msg)
 
+
+# 불린
 msg = True
 print(msg)
 
