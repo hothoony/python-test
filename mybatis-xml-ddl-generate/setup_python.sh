746c8bb940ccb6e695202fb3d2ff01f6ad2194ee
hothoony <hothoony@hanmail.net>
2025-06-10 15:10:24 +0900

    .

--- mybatis-xml-ddl-generate/setup_python.sh
+++ mybatis-xml-ddl-generate/setup_python.sh
@@ -2,18 +2,23 @@
 
 python3 --version
 
-python3 -m venv .venv
-source .venv/bin/activate
+# # 가상환경 비활성화
+# deactivate
+# rm -rf .venv
 
+# 가상환경 생성
+python3 -m venv .venv
+# 가상환경 활성화
+source ./.venv/bin/activate
+
+# 라이브러리 설치
 pip install pytest black flake8
 pip install dotenv
 pip install loguru
 pip install sqlparse
 
-pip list
+# 개발 모드로 패키지 설치 (pytest 로 테스트를 실행하기 위해 필요)
+pip install -e .
 
-# mkdir src/ tests/
-# touch src/__init__.py tests/__init__.py
-# touch src/main.py tests/test_main.py
-# touch requirements.txt
-# touch .gitignore .env README.md
+# 설치된 라이브러리 확인
+pip list
