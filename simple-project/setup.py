979f3987597b05b11781a22ae3314eb7e472d0f3
hothoony <hothoony@hanmail.net>
2025-06-10 14:32:29 +0900

    .

--- simple-project/setup.py
+++ simple-project/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_package",
+    name="my_package",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
