33b4d5a05142da00d5712c797a88ffe66523c104
hothoony <hothoony@hanmail.net>
2025-06-10 13:23:33 +0900

    .

---
+++ python-project-template/setup_python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python3 --version
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install pytest black flake8
+
+mkdir src/ tests/
+touch src/__init__.py tests/__init__.py
+touch src/main.py tests/test_main.py
+touch requirements.txt
+touch .gitignore .env README.md
