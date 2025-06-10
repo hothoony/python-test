24403d4b36232bdf4c757e21f32e9bbb663d2443
hothoony <hothoony@hanmail.net>
2025-06-10 15:46:32 +0900

    .

--- mybatis-xml-ddl-generate/tests/test_generate_ddl_find_all_xml_files.py
+++ mybatis-xml-ddl-generate/tests/test_generate_ddl_find_all_xml_files.py
@@ -2,6 +2,7 @@ import pytest
 from my_package.generate_ddl import find_all_xml_files
 
 def test_find_all_xml_files():
-    result = find_all_xml_files("/Volumes/mydata/office_work/project/ploonet_total/project_src/ploonet-total-backend/src/main/java/com/ploonet/total/backend/mapper")
-    print(len(result))
+    print()
+    result = find_all_xml_files("/Volumes/mydata/office_work/project/ploonet_total/project_src/ploonet-total-backend/src/main/resources/mapper")
+    print('result=',len(result))
     assert True is True
