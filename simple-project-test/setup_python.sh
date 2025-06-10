0556786a5a91bd87c4becbfb94554098aa30310b
hothoony <hothoony@hanmail.net>
2025-06-10 14:19:03 +0900

    .

---
+++ simple-project-test/setup_python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python3 --version
+
+# # 가상환경 비활성화
+# deactivate
+# rm -rf .venv
+
+# 가상환경 생성
+python3 -m venv .venv
+# 가상환경 활성화
+source ./.venv/bin/activate
+
+# 라이브러리 설치
+pip install pytest black flake8
+pip install dotenv
+pip install loguru
+
+# 설치된 라이브러리 확인
+pip list
