b64f54c4695db0f33ae742cff4525632c611c848
hothoony <hothoony@hanmail.net>
2025-06-11 17:45:09 +0900

    .

--- env-dev-prod/src/my_app/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-# 프로젝트 루트 디렉토리를 sys.path에 추가
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
--- env-dev-prod/tests/test_main.py
+++ env-dev-prod/tests/test_main.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-# Add the project root directory to the Python path
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
 import pytest
 from my_app.main import method01, method02, run
 
