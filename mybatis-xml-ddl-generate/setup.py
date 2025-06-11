e70ff993bc15de4d4f3386e58e2d4b6ba35ded72
hothoony <hothoony@hanmail.net>
2025-06-11 11:40:19 +0900

    .

--- mybatis-xml-ddl-generate/setup.py
+++ mybatis-xml-ddl-generate/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_app",
+    name="mybatis_xml_ddl_generate",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
